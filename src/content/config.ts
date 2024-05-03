823e8effda9b65513d1162ae7c09b0734b3df8b0
Broslunas <pablo.luna.perez.008@gmail.com>
2024-05-03 11:40:14 +0200

    add minecraft end xpansion draft

--- src/content/config.ts
+++ src/content/config.ts
@@ -31,6 +31,7 @@ const projects = defineCollection({
     draft: z.boolean().optional(),
     demoUrl: z.string().optional(),
     repoUrl: z.string().optional(),
+    downloadUrl: z.string().optional(),
   }),
 })
 
