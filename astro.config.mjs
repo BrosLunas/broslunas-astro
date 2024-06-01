f2edcc124952509d9b6e4c012f042ff0ddc44f87
Broslunas <pablo.luna.perez.008@gmail.com>
2024-06-01 11:02:55 +0200

    add analitics for vercel and move speed insights to the layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
   redirects: {
     '/projects/minecraft': '/projects',
     '/projects/juegos': '/projects',
-  }
+  },
+  // Vercel
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 })
