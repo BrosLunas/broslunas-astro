92ae8864ce69f80489bf8efae964f4b6de251ac1
Broslunas <pablo.luna.perez.008@gmail.com>
2024-06-01 11:06:47 +0200

    resolving bug off analitics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
-import solidJs from "@astrojs/solid-js"
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://broslunas.vercel.app",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  integrations: [mdx(), sitemap(), solidJs(), tailwind({
+    applyBaseStyles: false
+  })],
   redirects: {
     '/projects/minecraft': '/projects',
-    '/projects/juegos': '/projects',
+    '/projects/juegos': '/projects'
   },
-  // Vercel
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
-})
+  output: "server",
+  adapter: vercel()
+});
