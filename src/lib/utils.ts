d3478d122dc7ad399b470a9a930f1d1b84e3a76b
BrosLunas <pablo.luna.perez.008@gmail.com>
2024-07-20 16:59:12 +0200

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,9 +6,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: Date) {
-  return Intl.DateTimeFormat("en-US", {
-    month: "short",
+  return Intl.DateTimeFormat("es-ES", {
     day: "2-digit",
+    month: "short",
     year: "numeric"
   }).format(date)
 }
@@ -16,6 +16,6 @@ export function formatDate(date: Date) {
 export function readingTime(html: string) {
   const textOnly = html.replace(/<[^>]+>/g, "")
   const wordCount = textOnly.split(/\s+/).length
-  const readingTimeMinutes = ((wordCount / 200) + 1).toFixed()
-  return `${readingTimeMinutes} min read`
+  const readingTimeMinutes = ((wordCount / 200) + 2).toFixed()
+  return `${readingTimeMinutes} minutos de lectura`
 }
