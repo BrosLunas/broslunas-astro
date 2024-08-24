c0f061bcf59f2635ee8a7e25b8c31010400a6f52
Broslunas <83491031+BrosLunas@users.noreply.github.com>
2024-08-24 15:44:40 +0100

    fix erro header movile

---
+++ src/menu2.ts
@@ -0,0 +1,42 @@
+import type { Links } from "@types"
+
+// Links
+export const LINKS: Links = [
+    { 
+      TEXT: "Proyectos", 
+      HREF: "/projects",
+    },
+    { 
+      TEXT: "Blog", 
+      HREF: "/blog",
+    },
+    { 
+      TEXT: "Wikis",
+      HREF: "/wikis",
+    },
+    { 
+      TEXT: "Modpacks", 
+      HREF: "/modpack",
+    },
+    { 
+      TEXT: "Code", 
+      HREF: "https://code.broslunas.com/", 
+    },
+    { 
+      TEXT: "FileDrop", 
+      HREF: "https://filedrop.broslunas.com/", 
+    },
+    { 
+      TEXT: "AprendeJS", 
+      HREF: "https://aprendejs.broslunas.com/", 
+    },
+    { 
+      TEXT: "AiQr", 
+      HREF: "https://aiqr.broslunas.com/", 
+    },
+    { 
+      TEXT: "Uptime", 
+      HREF: "https://uptime.broslunas.com", 
+    },
+  ]
+  
