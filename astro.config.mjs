e3c286b932251a483dd255a61b4b5d045a8439a4
BrosLunas <pablo.luna.perez.008@gmail.com>
2024-08-25 13:22:32 +0200

    fix bugs analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,21 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://broslunas.com",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  integrations: [mdx(), sitemap(), solidJs(), 
+    tailwind({ 
+      applyBaseStyles: false 
+    }), 
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   redirects: {
     '/projects/minecraft': '/projects',
     '/projects/juegos': '/projects',
