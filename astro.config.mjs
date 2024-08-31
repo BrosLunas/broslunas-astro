cb118d0b73806cf2c245ccf473d72c91e9b44dda
Broslunas <pablo.luna.perez.008@gmail.com>
2024-08-31 10:26:21 +0100

    undo analitics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,10 @@ import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
 import partytown from '@astrojs/partytown'
-import vercel from '@astrojs/vercel/serverless';
 
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
   site: "https://broslunas.com",
   integrations: [mdx(), sitemap(), solidJs(), 
     tailwind({ 
